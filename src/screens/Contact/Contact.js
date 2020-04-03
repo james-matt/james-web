afec7c1fa1c0e9787b95e9ec3a21116d4589729a
james.taggart.43 <james.taggart.43@gmail.com>
2020-04-03 02:01:32 +0000

    Contact screen.
    Very basic contact page with email and platforms.
    
    MVP contact screen.

--- src/screens/Contact/Contact.js
+++ src/screens/Contact/Contact.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
+import Social from '../../components/Social/Social'
+import './Contact.css'
 
 class Contact extends React.Component {
     render () {
         return (
             <div>
-                <h1>Contact</h1>
+                <Header heading="Contact" image="http://via.placeholder.com/2000x600" />
+                <div className="contact-text-container">
+                    <div>
+                        <h2>But how do I contact you?</h2>
+                        <p>Send me a email! My email is <a href="mailto:james.taggart.43@gmail.com">james.taggart.43@gmail.com</a> or reach out to me on one of the platforms below.</p>
+                    </div>
+                </div>
+                <Social />
             </div>
         )
     }
