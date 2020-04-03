8df9b52b0c45e203a986a9a8421589cae71fcb5d
james.taggart.43 <james.taggart.43@gmail.com>
2020-04-03 01:08:54 +0000

    Cloud Screen
    Cloud screen with projects and technologies.
    
    Cloud screen

--- src/screens/Cloud/Cloud.js
+++ src/screens/Cloud/Cloud.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
+import Icons from '../../components/Icons/Icons'
+import Projects from '../../components/Projects/Projects'
+import Project from '../../components/Project/Project'
 
 class Cloud extends React.Component {
     render () {
         return (
             <div>
-                <h1>Cloud</h1>
+                <Header heading="Cloud" image="http://via.placeholder.com/2000x600" />
+                <Projects>
+                    <Project image="http://via.placeholder.com/500x300" heading="Kickstart Kids" description="Website with multiple environments, web servers, and storage." />
+                    <Project image="http://via.placeholder.com/500x300" heading="Sponge" description="App with multiple databases and pub &frasl; sub chat functionality." />
+                </Projects>
+                <Icons>
+                    <p>AWS</p>
+                    <p>Heroku</p>
+                    <p>PubNub</p>
+                </Icons>
             </div>
         )
     }
