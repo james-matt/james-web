3b296b5a0489f57181414ec08bba028e62fbac54
james.taggart.43 <james.taggart.43@gmail.com>
2020-04-07 02:08:27 +0000

    Pictures and flick.
    Working on getting content into the site.
    
    Site content.

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Flicking from '@egjs/react-flicking';
 import Social from '../../components/Social/Social'
+import WebImage from './web.png'
+import MobileImage from './mobile.png'
 import './Home.css'
 
 class Home extends React.Component {
@@ -8,9 +10,12 @@ class Home extends React.Component {
         return (
             <div className='home-container'>
                 <Flicking>
-                    <img alt='placeholder-1' src='http://via.placeholder.com/2000x600' />
-                    <img alt='placeholder-2' src='http://via.placeholder.com/2000x600' />
-                    <img alt='placeholder-3' src='http://via.placeholder.com/2000x600' />
+                    <div style={{ height: 600, width: 2000}}>
+                        <img alt='placeholder-1' src={WebImage}/>
+                    </div>
+                    <div style={{ height: 600, width: 2000}}>
+                        <img alt='placeholder-2' src={MobileImage}/>
+                    </div>
                 </Flicking>
                 <div className='home-title'>
                     <h1>I build websites, mobile apps, and cloud solutions.</h1>
