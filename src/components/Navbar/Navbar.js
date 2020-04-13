cb2b9b24698be37ad773194b77b4259d9cc6c124
james.taggart.43 <james.taggart.43@gmail.com>
2020-04-13 23:24:57 +0000

    Logos and favicons.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min'
 import './Navbar.css'
+import Logo from '../../assets/images/logo/logo.png'
 
 
 class Navbar extends React.Component {
@@ -8,8 +9,8 @@ class Navbar extends React.Component {
         return (
             <nav className="primary-bg padding-lg container">
                 <ul className="container">
-                    <li><NavLink to='/home'>JT</NavLink></li>
-                    <li className='nav-pages'><NavLink to='/websites'>Websites</NavLink></li>
+                    <li><NavLink to='/home'><img src={Logo} alt="logo" /></NavLink></li>
+                    <li><NavLink to='/websites'>Websites</NavLink></li>
                     <li><NavLink to='/mobile'>Mobile</NavLink></li>
                     <li><NavLink to='/cloud'>Cloud</NavLink></li>
                     <li><NavLink to='/contact'>Contact</NavLink></li>
