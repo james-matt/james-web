30796ed1ce84bb637e489d469dace5f9f976b83c
james.taggart.43 <james.taggart.43@gmail.com>
2020-04-19 21:38:54 +0000

    Page preview media queries.
    Media queries that resize elements on the page.
    
    Page preview home page media queries.

--- src/components/PagePreview/PagePreview.js
+++ src/components/PagePreview/PagePreview.js
@@ -5,18 +5,23 @@ import './PagePreview.css'
 class PagePreview extends React.Component {
     render () {
         return (
-            <div className="container-wrap padding-bottom-lg">
-                <div className="container-wrap center">
-                    <img alt="placeholder-1" src={this.props.image} style={{ maxHeight: 300, maxWidth: '100%' }} />
-                </div>
-                <div>
-                    <h2>{this.props.title}</h2>
-                    <p className="padding-bottom-sm">{this.props.description}</p>
-                </div>
-                <div className="container-wrap padding-bottom-sm">
-                    <Link className="btn" to={this.props.link}>View {this.props.title}</Link>
+            <div class="container-wrap center">
+                <div className="container-wrap page-preview-container padding-bottom-lg">
+                    <div className="container-wrap center page-preview-image-container">
+                        <img alt="placeholder-1" src={this.props.image} style={{ maxHeight: 300, maxWidth: '100%' }} />
+                    </div>
+                    <div className="container-wrap center page-preview-text-container">
+                        <div className="container-wrap cols">
+                            <h2>{this.props.title}</h2>
+                            <p className="padding-bottom-md">{this.props.description}</p>
+                            <div>
+                                <Link className="btn" to={this.props.link}>View {this.props.title}</Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            
         )
     }
 }
