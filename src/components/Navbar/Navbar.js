33982d4e5876f8c61c65fdf9740a37bba7ffa337
james.taggart.43 <james.taggart.43@gmail.com>
2020-04-21 01:15:55 +0000

    Home changed.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ class Navbar extends React.Component {
         return (
             <nav className="primary-bg padding-lg container">
                 <ul className="container align-items-center">
-                    <li><NavLink to='/home'><img src={Logo1} alt="logo" /></NavLink></li>
+                    <li><NavLink to='/'><img src={Logo1} alt="logo" /></NavLink></li>
                     <li><NavLink to='/websites'>Websites</NavLink></li>
                     <li><NavLink to='/mobile'>Mobile</NavLink></li>
                     <li><NavLink to='/cloud'>Cloud</NavLink></li>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Home from './screens/Home/Home'
 import NoMatch from './screens/NoMatch/NoMatch'
 import Websites from './screens/Websites/Websites'
@@ -11,10 +11,7 @@ class Routes extends React.Component {
     render () {
         return (
             <Switch>
-                <Route exact path="/home" component={Home} />
-                <Route exact path="/">
-                    <Redirect to="/home" />
-                </Route>
+                <Route exact path="/" component={Home} />
                 <Route exact path="/websites" component={Websites} />  
                 <Route exact path="/mobile" component={Mobile} /> 
                 <Route exact path="/cloud" component={Cloud} />
