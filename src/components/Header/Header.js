7b568cb1916778e9cd8d34ec0d1afc6170cbb0f0
james.taggart.43 <james.taggart.43@gmail.com>
2020-04-22 01:23:16 +0000

    min image heights

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 class Header extends React.Component {
     render () {
         return (
-            <header className="container-wrap relative center align-items-center header padding-md">
+            <header className="container-wrap relative center align-items-center header padding-md header-image">
                 <img alt='placeholder' src={this.props.image} className="header-image" />
                 <div className="text-container primary-bg padding-md border">
                     <h1>{this.props.heading}</h1>
